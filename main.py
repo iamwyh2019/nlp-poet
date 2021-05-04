976fffd16d70bd477182fc13a6074deb88c90121
iamwyh2019 <799810767@qq.com>
2021-05-04 16:57:05 +0800

    Dataset and DataLoader

---
+++ main.py
@@ -0,0 +1,1 @@
+from utils.dataloader import Poet_Dataset
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,6 +1,12 @@
 import numpy as np
 import torch
-from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
 
-class Poet_dataset(data.Dataset):
-    pass
+class Poet_Dataset(Dataset):
+    def __init__(self, filepath):
+        with open(filepath, 'r', encoding = 'utf-8-sig') as f:
+            lines = f.readlines()
+            self.poems = [0] * len(lines)
+            for i,line in enumerate(lines):
+                aft = line.rstrip().replace("，", '#').replace("。", '#')
+                self.poems[i] = aft
